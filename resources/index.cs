ac760e16519611482dd7665cc8fbbe8f31208ac9
Jeanine L <47186796+jalcoding8@users.noreply.github.com>
2025-12-27 15:40:41 -0800

    Refactor index.cs with new CSS styles
    Updated CSS styles and removed outdated content.

---
+++ resources/index.cs
@@ -0,0 +1,30 @@
+h1, h2, h3, p {
+
+    font-family: Helvetica, sans-serif;
+}
+
+p {
+    font-size: 18px;
+}
+
+h2 {
+    padding: 5px;
+    font-family: Helvetica, sans-serif;
+}
+  
+.brushes {
+    font-size: 42px;
+    font-weight: bold;
+    padding: 10%;
+}
+
+  
+.frames {
+    font-size: 40px;
+    font-weight: bold;
+}
+
+.paint {
+    font-size: 40px;
+    font-weight: bold;
+}
